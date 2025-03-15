9dfe224d00cf7d942aa7bce23be6430e533425f4
Dylannng12 <dylanegarcia387@gmail.com>
2025-03-15 16:31:58 -0300

    Actualizaciones en el FrontEnd(observar fts)

--- demo/src/main/resources/static/ZurJS.js
+++ demo/src/main/resources/static/ZurJS.js
@@ -1,28 +1,101 @@
 let img = document.querySelector(".imgsocial");
 
+let itemimg = document.querySelectorAll(".imgitem");
+ 
+
+itemimg.forEach((item)=>{
+
+
+   item.addEventListener("click",()=>{
+
+    const newImg = document.createElement("img");
+      dvimg.style.width="100%"
+    newImg.src= item.src; 
+    newImg.style.width = "100%"; // Tamaño más grande
+    newImg.style.maxHeight="550px";
+    dvenrm.style.display="block";   
+
+    
+    dvimg.innerHTML="";
+   
+    dvimg.appendChild(newImg);
+
+   })
+
+})
+
+
+
 let imgmostrar = document.querySelector(".imgmostrar");
 let dvimg = document.querySelector(".dvimg");
 img.addEventListener("click",()=>{
+
+
+  window.location.href="ZurInicio-1.html#imgmost";
 
     imgev();
 
   })
 
+const dvenrm = document.querySelector(".dvenrm");
+
   function imgev(){
+    
+    let heig =window.innerHeight ;
+     
+    let  resultt= heig - (heig * 0.8);
 
     const newImg = document.createElement("img");
-    newImg.src = img.src; // Copiar la imagen
-    newImg.style.width = "100%"; // Tamaño más grande
 
-    imgmostrar.style.display="block";
-    imgmostrar.style.transition="1s ";
-    imgmostrar.style.width="70%";
     
-    dvimg.style.width="100%";
+    if(window.innerWidth>=1100){
+      newImg.src = img.src; // Copiar la imagen
+    newImg.style.width = "100%"; // Tamaño más grande
+    newImg.style.maxHeight="550px";
+
+       dvimg.style.width="100%"
+     
+    console.log("entro al if");
+
 
     dvimg.innerHTML="";
    
-    dvimg.appendChild(newImg);
+  dvimg.appendChild(newImg);
+
+    }else if(window.innerWidth<1000){
+      
+
+  window.location.href="ZurInicio-1.html#dvimg";
+
+      const newImg = document.createElement("img");
+      dvimg.style.width="80%"
+      newImg.src = img.src; // Copiar la imagen
+    newImg.style.width = "90%"; // Tamaño más grande
+    newImg.style.height="auto";
+    
+
+
+    dvimg.innerHTML="";
+   
+  dvimg.appendChild(newImg);
+      console.log("entro al else");
+
+    }
+ 
+    dvenrm.style.display="block";   
+    
+ 
+
+    imgmostrar.style.display="flex";
+    imgmostrar.style.transition="1s ";
+    imgmostrar.style.width="auto";
+    
+    //dvimg.style.width="100%";
+
+
+   
+  console.log("Height"+window.innerHeight);
+  console.log("Width"+window.innerWidth);
 
   }
 
@@ -30,8 +103,11 @@ img.addEventListener("click",()=>{
 
   cerrarIMG.addEventListener("click",()=>{
 
-    imgmostrar.style.display="none";
+    window.location.href="ZurInicio-1.html#portfolio";
 
+    imgmostrar.style.display="none";
+    dvenrm.style.display="none";   
+  
 
   })
 
