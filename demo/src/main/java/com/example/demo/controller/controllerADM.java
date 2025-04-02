f378f7c3765d3b3e6edc1ea5f0b4f7e339eb5d67
=Dylannng12 <dylanegarcia387@gmail.com>
2025-04-02 11:47:26 -0300

    PR

--- demo/src/main/java/com/example/demo/controller/controllerADM.java
+++ demo/src/main/java/com/example/demo/controller/controllerADM.java
@@ -1,5 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.Entity.Userr;
+import com.example.demo.Service.UserService;
+import com.example.demo.config.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/adm")
 public class controllerADM {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public String adm() {
