472f7dff82aea73c1bbadf2a5a4a450ddd5ccc11
Dylannng12 <dylanegarcia387@gmail.com>
2025-07-31 22:29:55 -0300

    Update CnlControlller.java

--- demo/src/main/java/com/example/demo/controller/CnlControlller.java
+++ demo/src/main/java/com/example/demo/controller/CnlControlller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@CrossOrigin(origins = "https://repozur-1.onrender.com/adm")//poner url de la seccion de administrador
+@CrossOrigin(origins = "https://spring-app-53939419120.us-central1.run.app/adm?continue")//poner url de la seccion de administrador
 @RestController
 @RequestMapping("/VerConsutlas")
 public class CnlControlller {
